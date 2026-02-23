d782a99e679bab4f8c70e9b9a8a0122e25dd412c
Evandro Andrade <evandro_j.o.a@hotmail.com>
2026-02-23 06:56:44 -0300

    cirando nova tabelas e regras de gestão

--- src/context/AtendimentoContextoV2.jsx
+++ src/context/AtendimentoContextoV2.jsx
@@ -14,7 +14,7 @@
  */
 
 import React, { useReducer, useCallback } from 'react';
-import produce from 'immer';
+import { produce } from 'immer';
 
 const AtendimentoContextoV2 = React.createContext(null);
 
